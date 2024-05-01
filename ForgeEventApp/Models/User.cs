a24f060422d9182ea4f254aa6e9f902c7d46b70a
Lowkione <danielbeastgame@gmail.com>
2024-05-01 09:23:16 +0200

    Added Model Folder and User Model, Need to fix Hash + Salt Passowrd

---
+++ ForgeEventApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ForgeEventApp.Models
+{
+    public enum Role
+    {
+        User,
+        Admin
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public Role Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string HashPassword { get; set; }
+    }
+}
