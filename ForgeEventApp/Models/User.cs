aa289ad642225f97d340cf5043d77b34987f0c1b
Lowkione <danielbeastgame@gmail.com>
2024-05-02 12:47:22 +0200

    Created User with Salt

--- ForgeEventApp/Models/User.cs
+++ ForgeEventApp/Models/User.cs
@@ -15,6 +15,6 @@
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
-        public string HashPassword { get; set; }
+        public string Salt { get; set; }
     }
 }
