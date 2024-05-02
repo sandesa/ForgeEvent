b4fc31cf58b25f617e7a2f9dbfe4cb27c9eddf6d
Lowkione <danielbeastgame@gmail.com>
2024-05-02 18:52:47 +0200

    Required with error added

--- ForgeEventApp/Models/User.cs
+++ ForgeEventApp/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace ForgeEventApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForgeEventApp.Models
 {
     public enum Role
     {
@@ -9,12 +11,19 @@
     public class User
     {
         public int Id { get; set; }
-        public required Role Role { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required string Email { get; set; }
-        public string? PhoneNumber { get; set; }
-        public required string Password { get; set; }
-        public string? Salt { get; set; }
+        [Required(ErrorMessage = "Error R")]
+        public Role Role { get; set; }
+        [Required(ErrorMessage = "Required first name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Required last name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Required email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Required phonenumber")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Required password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Error S")]
+        public string Salt { get; set; }
     }
 }
