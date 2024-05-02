9e4c3798949924a0548c1021879d118d86119baf
Sampe <samuel.sandenas123@gmail.com>
2024-05-02 16:04:25 +0200

    I made most fields required and some fields nullable.

--- ForgeEventApp/Models/User.cs
+++ ForgeEventApp/Models/User.cs
@@ -9,12 +9,12 @@
     public class User
     {
         public int Id { get; set; }
-        public Role Role { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
-        public string Salt { get; set; }
+        public required Role Role { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public required string Password { get; set; }
+        public string? Salt { get; set; }
     }
 }
