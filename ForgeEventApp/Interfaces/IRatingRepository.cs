25fe0dac8a7a55424104056456be4aa0accb462b
Sampe <samuel.sandenas123@gmail.com>
2024-05-08 14:37:30 +0200

    Added GET GET for getting all ratings for a event

--- ForgeEventApp/Interfaces/IRatingRepository.cs
+++ ForgeEventApp/Interfaces/IRatingRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace ForgeEventApp.Interfaces
+﻿using ForgeEventApp.Models;
+
+namespace ForgeEventApp.Interfaces
 {
 	public interface IRatingRepository
 	{
+		Task<IEnumerable<Rating>> GetAllRatingsForEventAsync(int id);
 	}
 }
--- ForgeEventApp/Repositories/RatingRepository.cs
+++ ForgeEventApp/Repositories/RatingRepository.cs
@@ -1,9 +1,11 @@
 ﻿using ForgeEventApp.Data;
 using ForgeEventApp.Interfaces;
+using ForgeEventApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ForgeEventApp.Repositories
 {
-	public class RatingRepository : IRatingRepository 
+	public class RatingRepository : IRatingRepository
 	{
 		private readonly AppDbContext _context;
 
@@ -12,6 +14,12 @@ namespace ForgeEventApp.Repositories
 			_context = context;
 		}
 
+		public async Task<IEnumerable<Rating>> GetAllRatingsForEventAsync(int id)
+		{
+			var ratings = await _context.Ratings.Where(r => r.Event.Id == id).ToListAsync();
+
+			return ratings is null ? throw new InvalidOperationException($"Cannot find ratings for event with ID {id}") : ratings;
+		}
 
 	}
 }
