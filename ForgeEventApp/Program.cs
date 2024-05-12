70c624a9c565c8c64cfebb757070e6f66c627c7f
Sampe <samuel.sandenas123@gmail.com>
2024-05-12 10:09:19 +0200

    Updates: Navbar - added a user link and icon Home - made the cards links (link to details page about the event)
    I also added a package called BlazorBootstrap for easier styling.

--- ForgeEventApp/Program.cs
+++ ForgeEventApp/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
