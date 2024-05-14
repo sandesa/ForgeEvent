a4a6ca98801c6ffaee0ea01147e6bbb3fa8b5257
Lowkione <danielbeastgame@gmail.com>
2024-05-14 20:19:50 +0200

    Fixed Category Enum and Catagory for display. Fixed endpoints for Home and EventDetailPage

--- ForgeEventApp/Repositories/EventRepository.cs
+++ ForgeEventApp/Repositories/EventRepository.cs
@@ -2,6 +2,7 @@
 using ForgeEventApp.Interfaces;
 using ForgeEventApp.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections;
 
 namespace ForgeEventApp.Repositories
 {
@@ -32,7 +33,6 @@ namespace ForgeEventApp.Repositories
             return ev?.Price ?? 0;
         }
 
-
 		public async Task CreateEventAsync(Event events)
 		{
 			Event newEvent = new()
@@ -55,5 +55,72 @@ namespace ForgeEventApp.Repositories
         {
             return await _context.Events.Include(e => e.User).FirstOrDefaultAsync(e => e.Id == eventId);
         }
-}
+
+		public async Task<IEnumerable<Event>> GetEventByCategoryAsync(Category category)
+		{
+            return _context.Events.Where(e => e.Category == category);
+        }
+
+        //private static readonly Dictionary<Category, string> DisplayNames = new Dictionary<Category, string>
+        //{
+        //	{ Category.Music, "Music" },
+        //	{ Category.Technology, "Technology" },
+        //	{ Category.Food_And_Drinks, "Food & Drinks" },
+        //	{ Category.Sports, "Sports" },
+        //	{ Category.Art_And_Culture, " Art % Culture" },
+        //	{ Category.Fashion, "Fashion" },
+        //	{ Category.Comedy, "Comedy" },
+        //	{ Category.Film, "Film" }
+        //};
+
+        //      public Dictionary<Category, string> CategoryDisplayNames => DisplayNames;
+
+        //public async Task<Dictionary<Category, string>> GetCategoryAsync()
+        //{
+            
+           
+        //    return new Dictionary<Category, string>
+        //    {
+        //        { Category.Music, "Music" },
+        //        { Category.Technology, "Technology" },
+        //        { Category.Food_And_Drinks, "Food & Drinks" },
+        //        { Category.Sports, "Sports" },
+        //        { Category.Art_And_Culture, "Art & Culture" },
+        //        { Category.Fashion, "Fashion" },
+        //        { Category.Comedy, "Comedy" },
+        //        { Category.Film, "Film" }
+        //    };
+        //}
+
+        public Task<Dictionary<Category, string>> GetCategoryAsync()
+        {
+            return Task.FromResult(new Dictionary<Category, string>
+            {
+                { Category.Music, "Music" },
+                { Category.Technology, "Technology" },
+                { Category.Food_And_Drinks, "Food & Drinks" },
+                { Category.Sports, "Sports" },
+                { Category.Art_And_Culture, "Art & Culture" },
+                { Category.Fashion, "Fashion" },
+                { Category.Comedy, "Comedy" },
+                { Category.Film, "Film" }
+            });
+        }
+        private string GetDisplayName(Category category)
+        {
+            return category switch
+            {
+                Category.Music => "Music",
+                Category.Technology => "Technology",
+                Category.Food_And_Drinks => "Food & Drinks",
+                Category.Sports => "Sports",
+                Category.Art_And_Culture => "Art & Culture",
+                Category.Fashion => "Fashion",
+                Category.Comedy => "Comedy",
+                Category.Film => "Film",
+                _ => "Unknown"
+            };
+        }
+
+    }
 }
