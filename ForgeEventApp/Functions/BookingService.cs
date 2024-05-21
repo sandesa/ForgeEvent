44fc5e97acb6484eae07cdd282aa09b6ed785ceb
Sampe <samuel.sandenas123@gmail.com>
2024-05-21 03:28:12 +0200

    RatingPage completed. Some other minor changes.

--- ForgeEventApp/Functions/BookingService.cs
+++ ForgeEventApp/Functions/BookingService.cs
@@ -22,5 +22,23 @@ namespace ForgeEventApp.Services
             await _context.SaveChangesAsync();
             return true;
         }
+        public async Task<IEnumerable<Booking>> GetAllBookingsByUserIdAsync(int id)
+        {
+            var bookings = await _context.Bookings.Where(b => b.User.Id == id)
+                                .Include(b => b.Event)
+                                .Include(b => b.User)
+                                .ToListAsync();
+            return bookings;
+        }
+        public async Task<Booking> GetBookingById(int id)
+        {
+            var booking = await _context.Bookings.Where(b => b.User.Id == id)
+                                .Include(b => b.Event)
+                                .Include(b => b.User)
+                                .FirstOrDefaultAsync();
+
+            return booking is null ? throw new InvalidOperationException("Cannot find booking") : booking;
+
+        }
     }
 }
